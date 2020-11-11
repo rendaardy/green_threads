a9d2636736f0cd45ab63eac9ab27ee8e3cd323c6
Marenda S.A <renda_ardy@outlook.com>
2020-11-11 19:45:39 +0800

    refactor: remove comments

--- src/main.rs
+++ src/main.rs
@@ -147,9 +147,6 @@ impl Thread {
     }
 }
 
-// const SSIZE: isize = 48;
-// static mut S_PTR: *const u8 = 0 as *const u8;
-
 #[derive(Debug, Default)]
 #[repr(C)]
 struct ThreadContext {
@@ -161,28 +158,6 @@ struct ThreadContext {
     rbx: u64,
     rbp: u64,
 }
-
-// fn print_stack(filename: &str) {
-//     let mut f = std::fs::File::create(filename).unwrap();
-//     unsafe {
-//         for i in (0..SSIZE).rev() {
-//             writeln!(
-//                 f,
-//                 "mem: {}, val: {}",
-//                 S_PTR.offset(i as isize) as usize,
-//                 *S_PTR.offset(i as isize)
-//             )
-//             .expect("Error writing to file");
-//         }
-//     }
-// }
-
-// fn hello() -> ! {
-//     println!("I love waking up on a new stack!");
-//     print_stack("AFTER.txt");
-
-//     loop {}
-// }
 
 #[naked]
 #[inline(never)]
@@ -236,21 +211,4 @@ fn main() {
     });
 
     runtime.run();
-
-    // let mut ctx = ThreadContext::default();
-    // let mut stack = vec![0_u8; SSIZE as usize];
-    // let stack_ptr = stack.as_mut_ptr();
-
-    // unsafe {
-    // let stack_bottom = stack.as_mut_ptr().offset(SSIZE);
-    // let sb_aligned = (stack_bottom as usize & !15) as *mut u8;
-    // std::ptr::write(sb_aligned.offset(-16) as *mut u64, hello as u64);
-
-    // S_PTR = stack_ptr;
-    // std::ptr::write(stack_ptr.offset(SSIZE - 16) as *mut u64, hello as u64);
-    // print_stack("BEFORE.txt");
-    // ctx.rsp = stack_ptr.offset(SSIZE - 16) as u64;
-
-    // gt_switch(&mut ctx);
-    // }
 }
